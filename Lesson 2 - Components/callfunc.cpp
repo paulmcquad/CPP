9989328edc816763d4103e712a2b491bc3d80d11
Paul McQuade <paulmcquad@gmail.com>
2017-02-07 22:53:50 +0000

    Fixed EOL Conversion to Windows (CR LF)
    EOL garbled my github source tree.
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- Lesson 2 - Components/callfunc.cpp
+++ Lesson 2 - Components/callfunc.cpp
@@ -1,11 +1,28 @@
-#include <iostream>using namespace std; 
-// function Demonstrating a Call to a Function// prints out a useful message
-void DemonstrationFunction(){
- cout << "In Demonstration Function\n"; }
-// function main - prints out a message, then// calls DemonstrationFunction, then prints out// a second message.
-int main(int argc, char *argv[]) {
- cout << "In main\n" ;
- DemonstrationFunction();
- cout << "Back in main\n";
- return 0;
-}
+#include <iostream>
+using namespace std; 
+
+// function Demonstrating a Call to a Function
+// prints out a useful message
+
+void DemonstrationFunction(){
+
+ cout << "In Demonstration Function\n";
+
+ }
+
+// function main - prints out a message, then
+// calls DemonstrationFunction, then prints out
+// a second message.
+
+int main(int argc, char *argv[]) {
+
+ cout << "In main\n" ;
+
+ DemonstrationFunction();
+
+ cout << "Back in main\n";
+
+ return 0;
+
+}
+
--- Lesson 2 - Components/comments.cpp
+++ Lesson 2 - Components/comments.cpp
@@ -1,9 +1,22 @@
-#include <iostream>using namespace std;
- int main(int argc, char *argv[]) {
- /* this is a c-style comment	  and it extends until the closing	  star-slash comment mark */
- cout << "Hello World!\n";
- // this c++-style comment ends at the end of the line
- cout << "That comment ended!";
- // double slash comments can be alone on a line /* as can slash-star comments */
- return 0;
-}
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char *argv[]) {
+
+ /* this is a c-style comment
+	  and it extends until the closing
+	  star-slash comment mark */
+
+ cout << "Hello World!\n";
+
+ // this c++-style comment ends at the end of the line
+
+ cout << "That comment ended!";
+
+ // double slash comments can be alone on a line
+ /* as can slash-star comments */
+
+ return 0;
+
+}
+
